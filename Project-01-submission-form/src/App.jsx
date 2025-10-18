db36a85697dca6c263326576e43841e8ae52ac30
techieshruti <shrutichandra721@gmail.com>
2025-10-18 23:30:04 +0530

    added buttons

--- Project-01-submission-form/src/App.jsx
+++ Project-01-submission-form/src/App.jsx
@@ -23,6 +23,7 @@ const App = () => {
       <div>
       <label >Your Best Subjects</label><br/>
       <input type='checkbox'id='English'/>
+      <label for="English">English</label>
       <input type='checkbox' id='math'/>
       <label for="math">Maths</label>
       <input type='checkbox' id='science'/>
