860f746fe74cbeadbce0143b21c1fafec4f0d72e
techieshruti <shrutichandra721@gmail.com>
2025-10-19 20:56:08 +0530

    added Mockdata

--- Project_02_User_Profile_card/src/main.jsx
+++ Project_02_User_Profile_card/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Card from './App.jsx'
+// import {Mockdata} from './assets/Mockdata.jsx';
 
 createRoot(document.getElementById('root')).render(
    <>
-   <Card/>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
    </>
+   
 )
