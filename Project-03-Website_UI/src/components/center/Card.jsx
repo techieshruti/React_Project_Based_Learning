b93dea7b9a9c8a5e9848ac8e65a553796f917fdf
techieshruti <shrutichandra721@gmail.com>
2025-10-31 14:06:46 +0530

    added card component

--- Project-03-Website_UI/src/components/center/Card.jsx
+++ Project-03-Website_UI/src/components/center/Card.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import Image from '../../assets/bread.jpg'
-import {Button} from '../upper/Button.jsx'
+import Button from '../upper/Button.jsx'
 
 export const Card = () => {
   return (
     <div>
-        <div className='bg-blue-200 w-80 flex flex-col items-center'>
+        <div className='bg-[#faf5e6] w-80 flex flex-col items-center pb-4'>
         <div className='h-40 w-full'>
             <img className='h-full w-full object-cover' src={Image}/>
         </div>
         <h1 className='text-3xl font-bold pt-4'>Bread</h1>
-        <p className='px-10 py-4 mb-2'>ingredients to bake a wide variety of delicious breads and sweet pastries</p>
-        <p>ffff</p>
-        <Button/>
+        <p className='px-10 py-2 '>ingredients to bake a wide variety of delicious breads and sweet pastries</p>
+        <Button />
       </div>
     </div>
   )
--- Project-03-Website_UI/src/components/center/CenterCards.jsx
+++ Project-03-Website_UI/src/components/center/CenterCards.jsx
@@ -4,7 +4,7 @@ import { Card } from './Card'
 export const CenterCards = () => {
   return (
     // card container
-    <div className='bg-amber-800 h-screen flex justify-center gap-10'>
+    <div className='h-screen flex justify-center gap-10'>
       {/* card 1 */}
       <Card/>
       <Card/>
