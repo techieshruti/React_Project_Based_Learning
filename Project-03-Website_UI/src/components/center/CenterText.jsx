3e75145c607a2e70bd06bbe504a783aa0e1c2ba4
techieshruti <shrutichandra721@gmail.com>
2025-11-01 17:29:27 +0530

    adding responsiveness

--- Project-03-Website_UI/src/components/center/CenterText.jsx
+++ Project-03-Website_UI/src/components/center/CenterText.jsx
@@ -3,13 +3,13 @@ import { HandPlatter } from 'lucide-react';
 
 export const CenterText = () => {
   return (
-    <div className='flex flex-col items-center justify-center px-40 py-6'>
+    <div className='flex flex-col items-center justify-center px-6 sm:px-20 md:px-40 text-center py-6'>
         {/* icon */}
-        <HandPlatter className='w-20 h-20 text-[#a36033] drop-shadow-xl'/>
+        <HandPlatter className='w-16 h-16 sm:w-20 sm:h-20 text-[#a36033] drop-shadow-xl'/>
         {/* heading */}
-        <h1 className='text-3xl font-bold tracking-wide py-2'>Why Choose Us?</h1>
+        <h1 className='text-2xl sm:text-3xl font-bold tracking-wide py-2'>Why Choose Us?</h1>
         {/* paragraph */}
-        <p className='py-2 text-lg'>Our bakery is a place where tradition meets taste. We use only the freshest ingredients to bake a wide variety of delicious breads and sweet pastries every day. Choose us for quality, flavor, and a genuine love for baking that you can taste in every single bite.</p>
+        <p className='py-2 text-base sm:text-lg leading-relaxed'>Our bakery is a place where tradition meets taste. We use only the freshest ingredients to bake a wide variety of delicious breads and sweet pastries every day. Choose us for quality, flavor, and a genuine love for baking that you can taste in every single bite.</p>
     </div>
   )
 }
