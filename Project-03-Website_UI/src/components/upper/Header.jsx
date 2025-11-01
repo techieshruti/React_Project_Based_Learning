0093728cd511c27e73b08bde536faea0f40ba3f4
techieshruti <shrutichandra721@gmail.com>
2025-11-01 16:42:43 +0530

    adding responsiveness

--- Project-03-Website_UI/src/components/upper/Header.jsx
+++ Project-03-Website_UI/src/components/upper/Header.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { ChefHat } from 'lucide-react';
+import React, {useState} from 'react'
+import { ChefHat, Menu, X } from 'lucide-react';
 import Button from './Button';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className='bg-[#f7ebca] flex justify-around items-center px-12 py-3 sticky top-0 z-10'>
-        <div className='flex flex-col items-center font-bold text-2xl cursor-pointer'>
-        <ChefHat className='text-[#a36033] drop-shadow-xl w-10 h-10'/>
+    <header className='bg-[#f7ebca] sticky top-0 z-10 shadow-md'>
+    <div className='flex justify-between items-center px-6 py-4'>
+    
+    {/* logo */}
+        <div className='flex flex-col items-center font-bold text-2xl cursor-pointer sm:text-2xl'>
+        <ChefHat className='text-[#a36033] drop-shadow-xl w-8 h-8 sm:w-10 sm:h-10'/>
         <h2>THE RUSTIC OVEN</h2>
         </div>
-        <ul className='flex justify-around gap-x-8 font-medium text-lg cursor-pointer'>
+
+        {/* Desktop Nav */}
+        <ul className='hidden md:flex gap-x-8 font-medium text-lg cursor-pointer'>
             <li>Home</li>
             <li>About Us</li>
             <li>Products</li>
             <li>Contact</li>
         </ul>
-        <Button />
+        
+        {/* Mobile Toggle */}
+        <div className="flex items-center gap-4 md:hidden">
+          <Button />
+          <button onClick={() => setOpen(!open)}>
+            {open ? <X size={26} /> : <Menu size={26} />}
+          </button>
+        </div>
     </div>
-  )
+    </header>
+  );
 }
