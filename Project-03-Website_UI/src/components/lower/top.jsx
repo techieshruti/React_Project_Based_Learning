008c246ad2302eac5b44cf592474ed2dcfed9e31
techieshruti <shrutichandra721@gmail.com>
2025-11-02 23:55:43 +0530

    added section 1 in lower component

--- Project-03-Website_UI/src/components/lower/top.jsx
+++ Project-03-Website_UI/src/components/lower/top.jsx
@@ -7,8 +7,8 @@ export const Top = () => {
         {/* left div */}
             <div className='bg-red-200 w-1/3 h-full'>
               <h1>Visit Us Today</h1>
-              <h3></h3>
-              <p></p>
+              <h3>Taste the Difference in every bite</h3>
+              <p>Step into our cozy bakery and let the aroma of freshley baked goods welcome you. From our signature Artisian breads to our dceedants pastries and custum cakes, every item is crafted with finest ingredients and a passion for baking. we invite to explore our daily selectoins, enjoy a warm cup of coffee, and find your new favoriye treat. We can't wait to sare our love of baking with you.</p>
               <Button/>
             </div>
             {/* right div */}
