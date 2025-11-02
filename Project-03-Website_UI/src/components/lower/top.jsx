9c0b6892e4d02f483859aae3a9c168d080f79c4c
techieshruti <shrutichandra721@gmail.com>
2025-11-03 00:09:47 +0530

    added section 1 in lower component

--- Project-03-Website_UI/src/components/lower/top.jsx
+++ Project-03-Website_UI/src/components/lower/top.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import Button from '../upper/Button.jsx'
 
 export const Top = () => {
   return (
     <div className='relative bg-green-300 w-full h-1/2'>
         <section className='relative flex justify-center items-center gap-4 h-full'>
         {/* left div */}
-            <div className='bg-red-200 w-1/3 h-full'>
-              <h1>Visit Us Today</h1>
-              <h3>Taste the Difference in every bite</h3>
-              <p>Step into our cozy bakery and let the aroma of freshley baked goods welcome you. From our signature Artisian breads to our dceedants pastries and custum cakes, every item is crafted with finest ingredients and a passion for baking. we invite to explore our daily selectoins, enjoy a warm cup of coffee, and find your new favoriye treat. We can't wait to sare our love of baking with you.</p>
-              <Button/>
+            <div className='bg-red-200 w-1/3 h-full px-6 py-4'>
+              <h1 className='font-bold text-4xl italic py-2'>Visit Us Today</h1>
+              <h3 className='font-semibold text-shadow-md text-lg tracking-wider'>Taste the Difference in every bite</h3>
+              <p className='py-2'>Step into our cozy bakery and let the aroma of freshley baked goods welcome you. <br/> we invite to explore our daily selectoins, enjoy a warm cup of coffee, and find your new favoriye treat. We can't wait to sare our love of baking with you.</p>
+              <div className='py-4'>
+                <Button/>
+              </div>
             </div>
             {/* right div */}
             <div className='bg-blue-200 w-1/3 h-full'>right</div>
