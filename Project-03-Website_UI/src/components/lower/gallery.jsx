53c6e3885d9c675fd31e4cecf15801ecfce0abc7
techieshruti <shrutichandra721@gmail.com>
2025-11-04 01:47:28 +0530

    added section 2 in lower component

--- Project-03-Website_UI/src/components/lower/gallery.jsx
+++ Project-03-Website_UI/src/components/lower/gallery.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 export default function Gallery() {
   return (
     <div className='w-full h-1/2 relative flex justify-center'>
-      <section className='w-2/3 h-80'>
+      <section className='w-full h-80'>
         <div className='object-cover w-full h-full overflow-hidden flex justify-center my-2 gap-2'>
           <img
-            src='https://images.unsplash.com/photo-1756137945097-fd3c4d30a97b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
-            alt='img'
-          />
-          <img
-            src='https://images.unsplash.com/photo-1753796921649-cfaddf5a52ca?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=464'
-            alt='img'
-          />
-          <img
-            src='https://images.unsplash.com/photo-1693028846882-2db3b5233572?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=429'
+            src='https://images.unsplash.com/photo-1600383708782-42a91f5ac449?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
             alt='img'
           />
           <img
             src='https://images.unsplash.com/photo-1762032313029-7d799c618eae?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435'
+            alt='img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1627308595171-d1b5d67129c4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435'
+            alt='img'
+          />
+          <img
+            src='https://plus.unsplash.com/premium_photo-1670895802184-176bbb53a2f7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
             alt='img'
           />
           <img
