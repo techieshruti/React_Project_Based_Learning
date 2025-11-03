a63a7278c8c646fc7a15e74248ebabf136a690f2
techieshruti <shrutichandra721@gmail.com>
2025-11-04 00:55:43 +0530

    added section 2 in lower component

--- Project-03-Website_UI/src/components/lower/gallery.jsx
+++ Project-03-Website_UI/src/components/lower/gallery.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Gallery() {
   return (
-    <section>
-      <div className='bg-blue-300'>Gallery</div>
-    </section>
+    <div className='bg-pink-200 w-full h-1/2 relative flex justify-center'>
+      <section className='bg-green-500 w-2/3 h-full '>
+        <div className='bg-blue-200 my-2 h-full w-1/4 object-cover overflow-hidden'>
+          <img
+            className='bg-red-300'
+            src='https://img.freepik.com/free-photo/bright-neon-colors-shining-wild-chameleon_23-2151682779.jpg?semt=ais_hybrid&w=740&q=80'
+            alt='img'
+          />
+        </div>
+      </section>
+    </div>
   )
 }
