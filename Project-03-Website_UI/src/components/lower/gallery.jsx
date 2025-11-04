65129064d646da970dd60988ddd0eac651ee2e9e
techieshruti <shrutichandra721@gmail.com>
2025-11-04 17:07:13 +0530

    added section 2 in lower component

--- Project-03-Website_UI/src/components/lower/gallery.jsx
+++ Project-03-Website_UI/src/components/lower/gallery.jsx
@@ -22,6 +22,7 @@ export default function Gallery() {
           <img
             src='https://plus.unsplash.com/premium_photo-1670895802184-176bbb53a2f7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
             alt='img'
+            loading="lazy"
           />
           <img
             src='https://images.unsplash.com/photo-1620052087057-bfd8235f5874?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1528'
