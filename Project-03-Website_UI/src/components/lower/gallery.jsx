54733d9f9d33f2248fb6c63741a9a918d0079033
techieshruti <shrutichandra721@gmail.com>
2025-11-04 16:58:05 +0530

    added section 2 in lower component

--- Project-03-Website_UI/src/components/lower/gallery.jsx
+++ Project-03-Website_UI/src/components/lower/gallery.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 
 export default function Gallery() {
   return (
-    <div className='w-full h-1/2 relative flex justify-center bg-linear-to-t from-[#f7c99e] to-white'>
-      <section className='w-full h-80'>
-        <div className='object-cover w-full h-full overflow-hidden flex justify-center my-2 gap-2'>
+    <div className='w-full h-1/2 relative flex items-center justify-center bg-linear-to-t from-[#f7c99e] to-white'>
+      <section className='w-full h-75 lg:h-65'>
+        <div className='object-cover w-full h-full overflow-hidden flex justify-center my-4 gap-3'>
           <img
             src='https://images.unsplash.com/photo-1600383708782-42a91f5ac449?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
             alt='img'
+            loading="lazy"
           />
           <img
             src='https://images.unsplash.com/photo-1762032313029-7d799c618eae?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435'
             alt='img'
+            loading="lazy"
           />
           <img
             src='https://images.unsplash.com/photo-1627308595171-d1b5d67129c4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435'
             alt='img'
+            loading="lazy"
           />
           <img
             src='https://plus.unsplash.com/premium_photo-1670895802184-176bbb53a2f7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387'
             alt='img'
+            loading="lazy"
           />
           <img
             src='https://images.unsplash.com/photo-1620052087057-bfd8235f5874?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1528'
             alt='img'
+            loading="lazy"
           />
           
         </div>
--- Project-03-Website_UI/src/components/lower/top.jsx
+++ Project-03-Website_UI/src/components/lower/top.jsx
@@ -6,7 +6,7 @@ export const Top = () => {
     <div className='relative bg-linear-to-b from-[#f7c99e] to-white w-full h-1/2'>
         <section className='relative flex justify-center items-center gap-4 h-full'>
         {/* left div */}
-            <div className='bg-linear-to-b from-[#f5e2ae] to-white w-1/3 h-75 px-6 py-4 rounded-2xl '>
+            <div className='bg-linear-to-b from-[#f5e2ae] to-white w-1/3 h-75 px-6 py-4 rounded-2xl lg:h-70'>
               <h1 className='font-bold text-4xl italic py-2 '>Visit Us Today</h1>
               <h3 className='font-semibold text-shadow-md text-lg text-[#a36033] tracking-wider'>Taste the Difference in every bite</h3>
               <p className='pt-2'>Step into our cozy bakery and let the aroma of freshley baked goods welcome you. <br/> we invite to explore our daily selectoins, enjoy a warm cup of coffee, and find your new favoriye treat. We can't wait to sare our love of baking with you.</p>
@@ -15,7 +15,7 @@ export const Top = () => {
               </div>
             </div>
             {/* right div */}
-            <div className='w-1/3 h-75 flex justify-center gap-2'>
+            <div className='w-1/3 h-75 flex justify-center gap-2 lg:h-70'>
             {/* right div 1 */}
               <div className='w-full'>
               <img
